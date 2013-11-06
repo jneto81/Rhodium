a2baaf6bf38013337e570936b781bb266d2b627b
EwanValentine <ewan.valentine89@gmail.com>
2013-11-06 23:49:30 +0000

    Removed alloy folder

--- src/Alloy/Routes/Main.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-
-$app->match('/alloy/main', static function() use ( $app ) {
-	return new Response( 'Alloy/main' );
-});
