59e7b8e40822e462d37dcbd85e26ad54fb79c75a
Ewan Valentine <ewan.valentine89@gmail.com>
2013-11-25 11:38:07 +0000

    Clean up ready for merge

--- web/index_dev.php
+++ web/index_dev.php
@@ -9,7 +9,4 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = require __DIR__.'/../app/bootstrap.php';
 $app['debug'] = true;
 
-// Load the controllers
-// require __DIR__.'/../src/app.php';
-
 $app['http_cache']->run();
